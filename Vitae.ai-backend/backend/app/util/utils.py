23a1573b88b9270cee72a204d48acb1db1da0ed1
HenriqueSilvaXavier <134643923+HenriqueSilvaXavier@users.noreply.github.com>
2024-11-22 16:13:23 -0300

    Update utils.py

--- Vitae.ai-backend/backend/app/util/utils.py
+++ Vitae.ai-backend/backend/app/util/utils.py
@@ -1,7 +1,6 @@
 import openai
 import os
 import PyPDF2
-
 from dotenv import load_dotenv
 
 # Carrega as variáveis do arquivo .env
@@ -15,42 +14,53 @@ if not openai.api_key:
     raise ValueError("A chave da API do OpenAI não foi configurada. Verifique o arquivo .env ou as variáveis de ambiente.")
 
 # Função para análise do currículo
-def analyze_resume(content_text, area):
+def analyze_resume(content_text):
     try:
-        prompts = [
-            f"Analise o seguinte currículo e forneça dicas de como ele pode melhorar. Faça isso de forma breve, por meio de 3 frases e não liste informações que já tem no currículo, no formato texto corrido: {content_text}",
-            f"Diga como ele pode melhorar o apontamento de cada habilidade técnica no currículo e faça isso de forma breve, por meio de, no máximo, 2 frases, no formato texto corrido: {content_text}",
-            f"Sugira melhorias de como ele pode melhorar a forma de falar sobre as experiências profissionais. Faça isso de forma breve, usando no máximo 4 frases, no formato texto corrido: {content_text}"
-        ]
+        prompt = f"""
+        Aja como um especialista em recrutamento. 
+        Você vai analisar um currículo considerando os aspectos visuais.
+        Os critérios que deve analisar estão listados a seguir:
 
-        responses = []
-        for prompt in prompts:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "Você é um assistente que analisa currículos e fornece dicas de melhoria."},
-                    {"role": "user", "content": prompt}
-                ],
-                max_tokens=1024,
-                temperature=0.5
-            )
-            text = response.choices[0].message['content'].replace("\n", " ")
-            responses.append(text)
+        1. layout: o layout do currículo deve ser limpo, organizado e atraente, fácil de ler e seguir.
+        2. estrutura: deve ter espaçamento e margens uniformes para manter a consistência visual do currículo.
+        3. cores: deve ter uma paleta de cores coerente e profissional, como azul, cinza ou verde escuro.
+        4. fonte e tamanho: escolha uma fonte profissional e um tamanho legível, como Times New Roman ou Arial, em tamanho de 12 a 14.
 
-        return {
-            "estrutura_valida": verificar_estrutura(content_text),
-            "dicas_melhoria": responses[0],  # Texto corrido
-            "habilidades_tecnicas": responses[1],  # Texto corrido
-            "areas_experiencia": responses[2]  # Texto corrido
-        }
+        Sempre responda em formato JSON.
+        ## Sua resposta deve ser apenas o objeto JSON, seguindo as chaves: layout, estrutura, cores e fonte.
+        Em cada critério, adicione um criterio_detail, como layout_detail, detalhando por que o critério deve ser seguido.
+        Exemplo de resposta em formato JSON:
+        {{
+            "layout": "...",
+            "layout_detail": "...",
+            "estrutura": "...",
+            "estrutura_detail": "...",
+            "cores": "...",
+            "cores_detail": "...",
+            "fonte": "...",
+            "fonte_detail": "..."
+        }}
+
+        Análise do currículo:
+        {content_text}
+        """
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Você é um especialista em recrutamento que analisa currículos."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=1024,
+            temperature=0.5
+        )
+
+        # Extrai e retorna a resposta da IA
+        return response.choices[0].message['content']
+
     except Exception as e:
         return {"error": str(e)}
 
-
-# Função para verificar a estrutura do currículo
-def verificar_estrutura(curriculo):
-    tokens = curriculo.lower().split()  # Divide o texto por espaços e converte para minúsculas
-    return "experiência" in tokens and ("educação" in tokens or "formação" in tokens)
 
 # Função para processar o PDF e extrair texto
 def extract_text_from_pdf(file):
